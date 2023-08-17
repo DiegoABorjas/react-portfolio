dd08a76d3537714079685c7ca9bb8fcacbf58451
Diego <diegoborjas@gmail.com>
2023-08-16 23:56:53 -0500

    updated projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -5,15 +5,17 @@ import Project from "../components/Project"
 import noteTaker from "../assets/pictures/note-taker.png"
 import weather from "../assets/pictures/weather.jpg"
 import techBlog from "../assets/pictures/techblog.jpg"
+import yumYard from "../assets/pictures/yum-yard.png"
+import sportsBud from "../assets/pictures/sportsbud.jpg"
 
 export default function ProjectsPage() {
     const projects = [
         {
-            title: "tech-blog",
-            description: "CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well.",
-            image: techBlog,
-            github: "https://github.com/DiegoABorjas/14-mvc-tech-blog",
-            deployed: "https://mvc-tech-blog-db-f03ab6b04d13.herokuapp.com/",
+            title: "Yum Yum Yard",
+            description: "Cafe Management System with a menu displayed for customers, as well as a back-end login for the manager to perform CRUD operations on the menu.",
+            image: yumYard,
+            github: "https://github.com/zamorejake/YumYumYard",
+            deployed: "https://yums-4103a315e67a.herokuapp.com/",
         },
         {
             title: "note-taker",
@@ -23,11 +25,11 @@ export default function ProjectsPage() {
             deployed: "https://my-notes.herokuapp.com/",
         },
         {
-            title: "weather-app",
-            description: "A weather app that provides weather outlook for multiple cities.",
-            image: weather,
-            github: "https://github.com/DiegoABorjas/06-weather-dashboard",
-            deployed: "https://diegoaborjas.github.io/06-weather-dashboard/",
+            title: "SportsBud",
+            description: "Sports Social Networking Application where users could create/edit or join sports teams based on their location.",
+            image: sportsBud,
+            github: "https://github.com/DiegoABorjas/SportsBud",
+            deployed: "https://github.com/DiegoABorjas/SportsBud",
         },
         {
             title: "tech-blog",
